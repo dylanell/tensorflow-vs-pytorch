acaaf8962dd1a5dd69c0ab1559e69432aedec8ce
dylanell <dellio15@gmail.com>
2020-08-20 07:46:30 -0400

    made space in util for data helper functions

--- pytorch_train.py
+++ pytorch_train.py
@@ -6,32 +6,7 @@ import argparse
 import torch
 from torchvision.transforms import transforms
 from util.pytorch_dataset import image_dataset
-import pandas as pd
-
-def generate_lists_from_image_dataset(path):
-    # read train/test label files
-    train_labels_df = pd.read_csv('{}train_labels.csv'.format(path))
-    test_labels_df = pd.read_csv('{}test_labels.csv'.format(path))
-
-    # convert labels column to list
-    train_labels = train_labels_df['Label'].to_list()
-    test_labels = test_labels_df['Label'].to_list()
-
-    # convert filename column to list of absolute paths
-    train_files = train_labels_df['Image Filename'].map(lambda x: \
-        '{}train/{}'.format(path, x)).to_list()
-    test_files = test_labels_df['Image Filename'].map(lambda x: \
-        '{}test/{}'.format(path, x)).to_list()
-
-    # package data to dictionary
-    data_lists = {
-        'train_files': train_files,
-        'train_labels': train_labels,
-        'test_files': test_files,
-        'test_labels': test_labels,
-    }
-
-    return data_lists
+from util.data_helpers import generate_lists_from_image_dataset
 
 def main():
     # parse command line arguments
