ebc0d8bfda9fb515da13403c62bfe89d2ec97eb5
dylanell <dellio15@gmail.com>
2020-08-26 09:22:25 -0400

    changed comments

--- model/pytorch_classifier.py
+++ model/pytorch_classifier.py
@@ -7,31 +7,38 @@ import torch.nn.functional as F
 
 class Classifier(torch.nn.Module):
     """
-
+    TODO: Description.
     """
 
-    # initialize and define all layers within the model
+    # initialize and define all layers
     def __init__(self, in_channels, out_dimension):
         # run base class initializer
         super(Classifier, self).__init__()
 
         # define convolution layers
-        self.conv_1 = torch.nn.Conv2d(in_channels, 32, 3, stride=1, padding=1)  # in_dim / 1
-        self.conv_2 = torch.nn.Conv2d(32, 64, 3, stride=2, padding=1)           # in_dim / 2
-        self.conv_3 = torch.nn.Conv2d(64, 128, 3, stride=2, padding=1)          # in_dim / 2
-        self.conv_4 = torch.nn.Conv2d(128, 256, 3, stride=2, padding=1)         # in_dim / 2
+        self.conv_1 = torch.nn.Conv2d(
+            in_channels, 32, 3, stride=1, padding=1
+        )
+        self.conv_2 = torch.nn.Conv2d(
+            32, 64, 3, stride=2, padding=1
+        )
+        self.conv_3 = torch.nn.Conv2d(
+            64, 128, 3, stride=2, padding=1
+        )
+        self.conv_4 = torch.nn.Conv2d(
+            128, 256, 3, stride=2, padding=1
+        )
 
         # define fully connected layers
         self.fc_1 = torch.nn.Linear(256 * 4 * 4, out_dimension)
 
-    # compute forward propagation of input x using model layers (dynamic build)
+    # compute forward propagation of input x
     def forward(self, x):
         # compute output
-        z_1 = F.relu(self.conv_1(x))                                        # 32x32x32
-        z_2 = F.relu(self.conv_2(z_1))                                      # 64x16x16
-        z_3 = F.relu(self.conv_3(z_2))                                      # 128x8x8
-        z_4 = F.relu(self.conv_4(z_3))                                      # 256x4x4
-        z_4_flat = torch.flatten(z_4, start_dim=1)                          # 4096
-        z_5 = self.fc_1(z_4_flat)                                           # 10
-
+        z_1 = F.relu(self.conv_1(x))
+        z_2 = F.relu(self.conv_2(z_1))
+        z_3 = F.relu(self.conv_3(z_2))
+        z_4 = F.relu(self.conv_4(z_3))
+        z_4_flat = torch.flatten(z_4, start_dim=1)
+        z_5 = self.fc_1(z_4_flat)
         return z_5
